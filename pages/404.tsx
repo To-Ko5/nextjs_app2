85a8cc7285bdde910cdded2805872fbe20716e4a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-26 07:57:59 +0900

    Add: 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+const page404 = () => {
+  return (
+    <Layout title="404page">
+      <div>404page</div>
+    </Layout>
+  )
+}
+
+export default page404
