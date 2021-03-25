e14b19f3b10190c706ede864cf065bd7c1ad20ad
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-26 07:58:18 +0900

    Add: login page

---
+++ pages/login.tsx
@@ -0,0 +1,13 @@
+import { memo, VFC } from 'react'
+
+import Layout from '../components/Layout'
+
+const login: VFC = memo(() => {
+  return (
+    <Layout>
+      <div>ddd</div>
+    </Layout>
+  )
+})
+
+export default login
