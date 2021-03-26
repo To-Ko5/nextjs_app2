7d846d199d9dcbb516d7f6949176742f10e7546c
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-27 08:15:57 +0900

    Add: login時のメッセージを追加

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -3,10 +3,12 @@ import { useRouter } from 'next/router'
 import axios from 'axios'
 
 import { User } from '../types/api/user'
+import { useMessage } from './loginMessage'
 
 export const useAuth = () => {
   const [loading, setLoading] = useState(false)
   const router = useRouter()
+  const { showMessage } = useMessage()
 
   const login = useCallback(async (id: string) => {
     setLoading(true)
@@ -15,13 +17,15 @@ export const useAuth = () => {
         `https://jsonplaceholder.typicode.com/users/${id}`
       )
       if (response.status === 404) {
+        showMessage({ title: 'not login', status: 'error' })
         throw new Error('error')
       }
       if (response.status === 200) {
+        showMessage({ title: 'login', status: 'success' })
         router.push('/')
       }
     } catch {
-      alert('not login')
+      showMessage({ title: 'not login', status: 'error' })
     }
     setLoading(false)
   }, [])
